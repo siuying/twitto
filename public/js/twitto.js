07fd3eb5be71abf79bfe13fe186d86cff892bbf6
siuying <siu.ying@gmail.com>
2009-06-17 22:52:13 +0800

    formatting the page

--- public/js/twitto.js
+++ public/js/twitto.js
@@ -1,14 +1,10 @@
 var TwitTo = {};
 
-TwitTo.buildAction = function(actions) {
+TwitTo.buildActions = function(actions) {
     var actionHtml = [];
     for (var a in actions) {
         var action = actions[a];
-        console.log(action);
         actionHtml.push("<option value='" + action + ": '>" + action + ": </option>");
     }
-    
     $(actionHtml.join('')).appendTo("#action");;
 }
-
-
--- web.rb
+++ web.rb
@@ -1,9 +1,13 @@
 #!/usr/bin/env ruby
 require 'rubygems'
 require 'sinatra'
+require 'json'
+
 require 'lib/sinatra/oauth_auth'
 require 'lib/sinatra/twitter'
 require 'lib/sinatra/bitly'
+
+@@config = YAML.load_file("config.yml") rescue nil || {}
 
 configure do
   set :sessions, true
@@ -12,23 +16,28 @@ configure do
 end
 
 get '/' do
-  redirect '/go' if authorized?
   erb :index
 end
 
+# Allow user to input tweet
 get '/go' do
   authorize!
   @title      = params[:title] || ""
   @url        = params[:url]   || ""
-  @short_url  = bitly.shorten(@url).short_url
+  @short_url  = bitly.shorten(@url).short_url rescue @url
+  @actions    = JSON(@@config['actions']) 
   erb :go
 end
 
+# Accept user input and post them to Twitter
 post '/go' do
   authorize!
-  url     = params[:url] || ""
-  message = params[:message] || ""
-  twitter.update("Reading: #{message} (#{url})")
+  @url     = params[:url]     || ""
+  @message = params[:message] || ""
+  @action  = params[:action]  || ""
+
+  tweet = "#{@action} #{@message} (#{@url})"
+  twitter.update(tweet)
   erb :close
 end
 
