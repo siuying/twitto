60af581aac48cbe1cf3ccff84e1762d268b7e263
siuying <siu.ying@gmail.com>
2009-06-18 01:08:13 +0800

    fix twitter authentication

--- lib/sinatra/twitter.rb
+++ lib/sinatra/twitter.rb
@@ -5,13 +5,11 @@ require 'sinatra/base'
 module Sinatra
   module TwitterClient
     module Helpers
-      @@config = YAML.load_file("config.yml") rescue nil || {}
-
       def twitter
         raise "Require OauthAuth!" unless session
         return @twitter if @twitter
 
-        oauth = Twitter::OAuth.new(@@config['consumer_key'], @@config['consumer_secret'])
+        oauth = Twitter::OAuth.new(ENV['OAUTH_KEY'], ENV['OAUTH_SECRET'])
         oauth.authorize_from_access(session[:access_token] , session[:secret_token])
         @twitter = Twitter::Base.new(oauth)
       end
