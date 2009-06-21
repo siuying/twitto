ced40c94135bd3e1adabdc828cca458628bb7c6a
siuying <siu.ying@gmail.com>
2009-06-21 16:21:12 +0800

    fix config

--- public/js/twitto.js
+++ public/js/twitto.js
@@ -38,7 +38,7 @@ var TwitTo = {
     var actionHtml = [];
     for (var a in actions) {
         var action = actions[a];
-        actionHtml.push("<option value='" + action + "'>" + action + ": </option>");
+        actionHtml.push("<option value='" + action + "'>" + action + "</option>");
     }
     $(actionHtml.join('')).appendTo("#action");
   }
