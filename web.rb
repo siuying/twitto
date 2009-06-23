c02d89eaf3454d9ea237b30c2ecbcab069873e96
siuying <siu.ying@gmail.com>
2009-06-24 01:30:00 +0800

    need requires!

--- web.rb
+++ web.rb
@@ -3,11 +3,13 @@ require 'rubygems'
 require 'json'
 require 'sinatra/base'
 
-
+gem 'datamapper', '>= 0.9.11'
 gem 'data_objects', '>= 0.9.11'
 require 'data_objects'
 gem 'do_postgres', '>= 0.9.11'
 require 'do_postgres'
+require 'datamapper'
+
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{FileUtils.pwd}/twitto.db")
 DataMapper.auto_migrate!
 require 'lib/sinatra/twitto'
